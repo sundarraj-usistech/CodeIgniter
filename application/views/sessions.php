c823ca6f114d89605de943cac5f51a47e551291c
sundarraj-usistech <102951821+sundarraj-usistech@users.noreply.github.com>
2022-05-12 12:59:50 +0530

    Delete sessions.php

--- application/views/sessions.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>Code Igniter Flash Session</title>
-    </head>
-    <body>
-        <p>Logged in as <b> <?=$this->session->userdata('name');?> </b></p>
-        <p>This is a session message <?=$this->session->flashdata('msg');?></p>
-    </body>
-</html>

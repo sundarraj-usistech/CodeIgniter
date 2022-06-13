52bab8e526e9a0b4a255e78dc1a027813c1ec907
sundarraj.p <sundarraj@usistech.com>
2022-06-13 19:32:54 +0530

    updated

--- application/views/addUserView.php
+++ application/views/addUserView.php
@@ -45,13 +45,13 @@
 			  		<a href="javascript:window.history.go(-1);" style="text-decoration: none;"><button name="back" class="btn btn-dark">BACK</button></a>
 			  	</div>
 
-			  	<nav class="pb-5" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+			  <!-- 	<nav class="pb-5" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns= 'http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath' d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34';);" aria-label="breadcrumb">
 			      <ol class="breadcrumb">
 			        <li class="breadcrumb-item"><a class="fw-bold text-dark" href="<?= base_url(); ?>index.php/mainController/view">HOME</a></li>
 			        <li class="breadcrumb-item active" aria-current="page">Add Details</li>
 			      </ol>
 			    </nav>
-
+ -->
 				<form method="post" action="<?= base_url(); ?>index.php/mainController/addUser">
 
 					<table class="table table-borderless mt-5" align="center">          
